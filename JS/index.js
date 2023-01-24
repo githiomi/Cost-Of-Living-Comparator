f5ba089e3825af9bde1c8f01a5b8d144af42d3c2
Daniel Githiomi <danielgithiomi@gmail.com>
2023-01-24 13:16:23 +0400

    Completed first index page with no video background

---
+++ JS/index.js
@@ -0,0 +1,6 @@
+// For the type writer animation
+var type = new Typed(".typewriter", {
+    strings: ["comparator<span class='dollar'>_</span>"],
+    typeSpeed: 110,
+    backSpeed: 100,
+    loop: true});
