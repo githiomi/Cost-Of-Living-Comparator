f570c3318583e6097a43dd8cddc0cf9438ff2a76
Daniel Githiomi <danielgithiomi@gmail.com>
2023-02-12 21:29:45 +0400

    Created contact page and added CSS and JS

---
+++ JS/contact.js
@@ -0,0 +1,23 @@
+// Contact Page JS
+// Connect to the DB
+const connectBtn = document.getElementById("connect");
+
+connectBtn.addEventListener("click", (e) => {
+    // Prevent page from reloading
+    if (connectBtn.classList.contains("disabled")) {
+        alert("Already connected to the database! Don't click me.");
+    } else {
+        e.preventDefault();
+
+        // Show an alert dialog
+        alert("Connecting to the Database Server now...");
+
+        const status = document.querySelector('.status');
+        // Change text and color
+        status.innerHTML = "Now connected successfully :)";
+        status.style.color = "green";
+
+        // Disable connect button
+        connectBtn.classList.add("disabled");
+    }
+});
